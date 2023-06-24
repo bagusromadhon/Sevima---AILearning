717115f04f4e7a58f99dc0fc13a0e9e010c2b258
@bagusromadhon007 <bagusromadhon007@gmail.com>
2023-06-25 05:32:54 +0700

    [Create]Create Siswa Account

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -32,39 +32,3 @@ class SiswaController extends Controller
         return back()->with('success','Register successfullyi');
     }
 }
-
-// <?php
-
-// namespace App\Http\Controllers;
-// use App\Models\Siswa;
-// use Illuminate\Http\Request;
-// use Illuminate\Support\Facades\Hash;
-// use Illuminate\Validation\Rule;
-
-// class SiswaController extends Controller
-// {
-//     public function register()
-//     {
-//         return view('user.register.index');
-//     }
-//     public function registerPost(Request $request)
-//     {
-
-//         $request->validate([
-//             'username' => 'required',
-//             'email' => ['required', 'email', Rule::unique('siswas', 'email')->ignore($request->id)],
-//             'password' => 'required',
-//         ]);
-
-//         $siswa = new Siswa();
-
-//         $siswa->username=$request->username;
-//         $siswa->email=$request->email;
-//         $siswa->password = Hash::make($request->password);
-//         $siswa->kode_kelas = null;
-
-//         $siswa->save();
-//         return back()->with('success','Register successfullyi');
-//     }
-// }
-
